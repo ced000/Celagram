10244b6b50edece10114953660906c170c78bbef
ced000 <egboncedric@gmail.com>
2021-07-05 09:22:30 +0100

    Frontend done to a large extent.

--- Celo-blog/main.js
+++ Celo-blog/main.js
@@ -2,7 +2,7 @@
 const postObjectList = [
     {
         title: "Totti",
-        imageURL: "https://i.imgur.com/rne8QyY.jpg",
+        imageURL: "https://source.unsplash.com/300x300/?tortoise",
         desc: "Slowest pet alive",
         likes: 20,
         dislikes: 10,
@@ -10,7 +10,7 @@ const postObjectList = [
     },
     {
         title: "Cathy",
-        imageURL: "https://i.imgur.com/DspN52g.jpg",
+        imageURL: "https://source.unsplash.com/300x300/?cats",
         desc: "Cathy, the innocent",
         likes: 100,
         dislikes: 16,
@@ -18,7 +18,7 @@ const postObjectList = [
     },
     {
         title: "Puggy",
-        imageURL: "https://i.imgur.com/MA1PGYb.jpg",
+        imageURL: "https://source.unsplash.com/300x300/?pugs",
         desc: "Puggy, the cutest",
         likes: 250,
         dislikes: 3,
@@ -26,13 +26,29 @@ const postObjectList = [
     },
     {
         title: "Pat",
-        imageURL: "https://i.imgur.com/rTY4Xkl.png",
+        imageURL: "https://source.unsplash.com/300x300/?parrots",
         desc: "Pat, the talking companion",
         likes: 25,
         dislikes: 1,
         author: "https://twitter.com/afrocryptomania"
-    },
+    }
+    // {
+    //     title: "doggy",
+    //     imageURL: "https://source.unsplash.com/300x300/?dogs",
+    //     desc: "cuteness animated",
+    //     likes: 2,
+    //     dislikes: 1,
+    //     author: "https://twitter.com/afrocryptomania"
+    // }
 ];
+
+// When window loads...
+window.onload = () => {
+    counter = 0;
+    renderPost();
+    postContents = slide();
+    btnCheck();
+}
 
 // getting DOM elements...
 const uploadBtn = document.getElementById('upBtn');
@@ -40,6 +56,8 @@ const container = document.getElementById('formCont');
 const cancelBtn = document.getElementById('formCancel');
 const nextBtn = document.getElementById('nextBtn');
 const prevBtn = document.getElementById('prevBtn');
+const formUploadBtn = document.getElementById('formUpload');
+
 
 
 // handling button events...
@@ -51,25 +69,33 @@ cancelBtn.onclick = () => {
     container.classList.remove('visible')
 };
 
+const slide = () => {
+    const postSlide = document.querySelector('.post-slide')
+    const postContents = document.querySelectorAll('.post-slide .post-container');
+    const postWidth = postContents[0].clientWidth;
+    postSlide.style.transition = 'transform 0.4s ease-in-out';
+    postSlide.style.transform = 'translatex(' + (-postWidth * counter) + 'px)';
+
+    return postContents;
+}
+
 nextBtn.onclick = () => {
     counter++;
     btnCheck();
-    postSlide.style.transition = 'transform 0.4s ease-in-out';
-    postSlide.style.transform = 'translatex(' + (-postWidth * counter) + 'px)';
+    slide();
 }
 
 prevBtn.onclick = () => {
     counter--;
     btnCheck();
-    postSlide.style.transition = 'transform 0.4s ease-in-out';
-    postSlide.style.transform = 'translatex(' + (-postWidth * counter) + 'px)';
+    slide();
 }
 
 // handling button appearance...
 const btnCheck = () => {
     if (counter <= 0) {
         prevBtn.style.display = 'none';
-    } else if ( counter >= postContents.length - 1) {
+    } else if ( counter >= postContents.length-1) {
         nextBtn.style.display = 'none';
     } else {
         prevBtn.style.display = 'unset';
@@ -77,22 +103,12 @@ const btnCheck = () => {
     };
 }
 
-// When window loads...
-window.onload = () => {
-    counter = 0;
-    renderPost();
-    postSlide = document.querySelector('.post-slide');
-    postContents = document.querySelectorAll('.post-slide .post-container');
-    postWidth = postContents[0].clientWidth;
-    postSlide.style.transform = 'translatex(' + (-postWidth * counter) + 'px)';
-    btnCheck();
-}
 
 // Function to render posts from list on window load...
 
 const renderPost = () => {
     let postSlide = document.getElementById('postSlide');
-    if (postSlide.innerHTML) postSlide.innerHTML = '';
+    postSlide.innerHTML = '';
     postObjectList.forEach ((post) => {
         newDiv = document.createElement('div');
         newDiv.classList.add('post-container');
@@ -104,12 +120,12 @@ const renderPost = () => {
         </div>
         <ul>
             <li>
-                <button>👍</button>
-                <small>${post.likes}</small>
+                <button id = "likeBtn" onclick = upLike()>👍</button>
+                <small id = "noLikes">${post.likes}</small>
             </li>
             <li>
-                <button>👎</button>
-                <small>${post.dislikes}</small>
+                <button id = "dislikeBtn" onclick = upDislike() >👎</button>
+                <small id = "noDislikes">${post.dislikes}</small>
             </li>
             <li>
                 <button>📤</button>
@@ -118,8 +134,61 @@ const renderPost = () => {
         </ul>
         <a href="${post.author}" target="_blank">Author</a>
 `
-        postSlide.appendChild(newDiv);
+    postSlide.appendChild(newDiv);
     })
-
-    return postSlide;
 }
+
+const upLike = () => {
+    noLikes = postObjectList[counter].likes;
+    noLikes += 1;
+    postObjectList[counter].likes = noLikes;
+    postContents[counter].getElementsByTagName('small')['noLikes'].innerHTML = noLikes;
+}
+
+const upDislike = () => {
+    noDislikes = postObjectList[counter].dislikes;
+    noDislikes += 1;
+    postObjectList[counter].dislikes = noDislikes;
+    postContents[counter].getElementsByTagName('small')['noDislikes'].innerHTML = noDislikes;
+}
+
+
+
+const notification = (text) => {
+    const notification = document.querySelector('.notification');
+    const notLoader = document.querySelector('.not-loader');
+    document.querySelector('.not-text').innerHTML = text;
+    notification.style.transform = 'translatex(100%)';
+    notification.addEventListener('transitionend', () => {notLoader.style.transform = 'translatex(-100%)';});
+    setTimeout(() => {
+        notification.style.transform = 'translatex(-100%)';
+        notification.addEventListener('transitionend', () => {
+            notLoader.style.transform = 'unset';
+        });
+    }, 5000);
+}
+
+const addProduct = () => {
+    let petPost = {};
+    let name = document.getElementById('petName').value;
+    let imgUrl = document.getElementById('imgUrl').value;
+    let desc = document.getElementById('desc').value;
+    let author = document.getElementById('authorHandle').value;
+    // filling up petPost object. 
+    petPost.title = name;
+    petPost.imageURL = imgUrl;
+    petPost.desc = desc;
+    petPost.author = author;
+    petPost.likes = 0;
+    petPost.dislikes = 0;
+
+    postObjectList.push(petPost);
+    renderPost();
+    postContents = slide();
+    btnCheck();
+    container.classList.remove('visible');
+    notification('🚀 New pet added!');
+
+}
+
+formUploadBtn.onclick = addProduct;
